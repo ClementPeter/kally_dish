a7310e9666c3b71b9709f117d7c1156a69ae767e
ClementPeter <clementpeter2000@gmail.com>
2024-04-20 08:25:21 +0100

    🚀setup color pallete

--- lib/ui/common/app_colors.dart
+++ lib/ui/common/app_colors.dart
@@ -7,3 +7,24 @@ const Color kcMediumGrey = Color(0xFF474A54);
 const Color kcLightGrey = Color.fromARGB(255, 187, 187, 187);
 const Color kcVeryLightGrey = Color(0xFFE3E3E3);
 const Color kcBackgroundColor = kcDarkGreyColor;
+
+class AppColors {
+  static const Color gray1 = Color(0xffF6F6F6);
+  static const Color gray6 = Color(0xffD1D1D1);
+  static const Color gray7 = Color(0xffAEAEAE);
+  static const Color gray8 = Color(0xff8B8B8B);
+  static const Color gray9 = Color(0xff696969);
+  static const Color gray11 = Color(0xff2A2A2A);
+  static const Color gray12 = Color(0xFF292929);
+  static const Color primary6 = Color(0xffFD7014);
+  static const Color primary11 = Color(0xFF331604);
+  static const Color grey13 = Color(0xFFE8E8E8);
+  static const Color redShade = Color(0xFFCC444B);
+  static const Color popUpBg = Color(0xFFF5F5F5);
+  static const Color orangeShade = Color(0xFFFFF3EC);
+  static const Color barrierColor = Color(0x99454545);
+  static const Color iconBackground = Color(0x4BFFF3CE);
+  static const Color iconBackground2 = Color(0xFFF5F5F5);
+  static const Color iconBackground3 = Color(0x728B8B8B);
+  static const Color white = Color(0xFFFFFFFF);
+}
