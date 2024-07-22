37471faa90d6c1eaf53754c269988db6a9a6132b
ClementPeter <clementpeter2000@gmail.com>
2024-07-22 16:47:12 +0100

    login-main_merge_cleanup_1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:kally_dish/app/app.bottomsheets.dart';
 import 'package:kally_dish/app/app.dialogs.dart';
